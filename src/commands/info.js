b64ab8409cf18974c8b42223c5340dc09629fc0e
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-05-14 21:38:23 +0700

    update info

--- src/commands/info.js
+++ src/commands/info.js
@@ -3,8 +3,6 @@ import cli from 'commander';
 
 export default () => {
 	log.info( '=== Create WP Site v' + cli.version() + ' ===' );
-	log.info(
-    'Create WP Site is a site creation wizard for WordPress using 10up/wp-docker'
-  );
+	log.info( 'Create WP Site is a site creation wizard for WordPress using 10up/wp-local-docker' );
 	log.info( 'Run create-wp-site --help for more info' );
 };
